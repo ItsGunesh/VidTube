680cfd8edacc2a8733fa6ab715c14635e6c00aeb
ItsGunesh <itsme.gunesh87@gmail.com>
2025-05-25 11:08:26 +0530

    MongoDB connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello from Gunesh")
+import {app} from "./app.js"
+import dotenv from "dotenv"
+import cors from "cors"
+import connectDB from "./db/index.js"
+// import "dotenv/config"
+
+dotenv.config()
+
+const PORT = process.env.PORT || 7000
+
+// console.log(process.env.PORT)
+
+connectDB()
+.then(()=>{
+    app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+})
+.catch((err)=>{
+    console.log("MongoDB conncection error")
+})
