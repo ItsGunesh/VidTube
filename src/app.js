c75ef666409a9909f503f63756c8d233ff8e2441
ItsGunesh <itsme.gunesh87@gmail.com>
2025-06-10 13:12:29 +0530

    Updates

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
 
 
 const app = express()
@@ -10,8 +11,10 @@ app.use(
     })
 )
 
-app.use(express.json({liimit:"16kb"}))
+app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true , limit:"16kb"}))
 app.use(express.static("public"))
+
+app.use("/api/v1/healthcheck",healthcheckRouter)
 
 export {app}
